ed547e88538c5ce370cd6cc57f0fe4714b7edc9e
Angel <angel.corpuz.jr@gmail.com>
2016-12-20 11:07:59 +0100

    Initial script to modify user status

---
+++ set_user_status.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# set_user_status.sh
+#
+# ####################################################################
+# Description: 	shell script to lock/unlock a user. Script determines if
+#				user exists and recovers user data prom passwd. If user
+#				is locked it asks if it needs to be unlocked and vice
+#				versa.
+# Parameter: username to lock/unlock.
+#
+# --------------------------------------------------------------------
+# 2016 Sapienza - department of bioinformatics
+#
+# Created 20.12.2016 10:57:15 CET
+# ======== Changelog ========
+# 2016-12-20 bioangel <angel<dot>corpuz<dot>jr@gmail<dot>com>
+# * Initial script
+#
+#
+#
+# ####################################################################
